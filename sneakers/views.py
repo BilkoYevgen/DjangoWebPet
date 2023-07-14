130049dd25e7ffca4a7eeb2f50679da2391749e2
Yevgen Bilko <bilko.yevgen@gmail.com>
2023-07-14 17:17:01 +0300

    Registration and login added. (not finished)

--- sneakers/views.py
+++ sneakers/views.py
@@ -29,7 +29,21 @@ def contacts(request):
     return render(request, 'sneakers/contact.html')
 
 def products(request):
-    return render(request, 'sneakers/products.html')
+    all_brands = Brand.objects.all()
+    ten_brands = all_brands[:5]
+    next_brands = all_brands[5:10]
+    total_prod_female = get_product_count_by_gender('F', exclude_kids=True)
+    total_prod_male = get_product_count_by_gender('M', exclude_kids=True)
+    total_prod_kids = get_product_count_by_gender('U', exclude_kids=False)
+
+    context = {
+        'ten_brands': ten_brands,
+        'next_brands': next_brands,
+        'total_prod_female': total_prod_female,
+        'total_prod_male': total_prod_male,
+        'total_prod_kids': total_prod_kids,
+    }
+    return render(request, 'sneakers/products.html', context)
 
 def single(request):
     all_brands = Brand.objects.all()
--- users/views.py
+++ users/views.py
@@ -1,11 +1,36 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from users.models import User
+from users.forms import UserLoginForm, UserRegistrationForm
+from django.contrib import auth
+from django.urls import reverse
 
 # Create your views here.
 def login(request):
-    return render(request, 'users/login.html')
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+    context = {'form': form}
+    return render(request, 'users/login.html', context)
 
 def register(request):
-    return render(request, 'users/register.html')
+    if request.method == "POST":
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse("users:login"))
+    else:
+        form = UserRegistrationForm()
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
 
 def profile(request):
     return render(request, 'users/profile.html')
