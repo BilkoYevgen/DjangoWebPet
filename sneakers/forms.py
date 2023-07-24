261ecb6624e6974024e8beddb8c35ba8a90d4df5
Yevgen Bilko <bilko.yevgen@gmail.com>
2023-07-24 20:18:42 +0300

    Add form for contact.html but send_mail logic is not done yet

---
+++ sneakers/forms.py
@@ -0,0 +1,18 @@
+from sneakers.models import Product
+from django import forms
+
+from django import forms
+
+class ContactForm(forms.Form):
+    name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
+        'value': 'Name...', 'onfocus': "this.value='';", 'onblur': "if (this.value=='') {this.value='Name...';}"
+    }))
+    email = forms.EmailField(widget=forms.TextInput(attrs={
+        'value': 'Email...', 'onfocus': "this.value='';", 'onblur': "if (this.value=='') {this.value='Email...';}"
+    }))
+    subject = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
+        'value': 'Subject...', 'onfocus': "this.value='';", 'onblur': "if (this.value=='') {this.value='Subject...';}"
+    }))
+    message = forms.CharField(widget=forms.Textarea(attrs={
+        'onfocus': "this.value='';", 'onblur': "if (this.value=='') {this.value='Message';}"
+    }))
